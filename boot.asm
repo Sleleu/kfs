2131ee01a23de309d5acffa27a02290624b7d877
Sleleu <93100775+Sleleu@users.noreply.github.com>
2025-07-06 01:48:54 +0200

    add scroll

--- boot.asm
+++ boot.asm
@@ -1,9 +1,13 @@
 [bits 32]
 
+MAGIC_NUMBER equ 0x1BADB002
+FLAGS equ 0x0
+CHECKSUM equ -(MAGIC_NUMBER + FLAGS)
+
 section .multiboot          ; Multiboot specification
-    dd 0x1BADB002           ; Magic number '1 BAD BOOT'
-    dd 0x0                  ; Flag
-    dd -(0x1BADB002 + 0x0)  ; Checksum
+    dd MAGIC_NUMBER         ; Magic number '1 BAD BOOT'
+    dd FLAGS                ; Flag
+    dd CHECKSUM             ; Checksum
 
 section .text               ; Section for executable instructions
 global start
@@ -15,7 +19,10 @@ start:
     call kernel_main        ; Call kernel_main function
     hlt                     ; Halt the CPU
 
+
+KERNEL_STACK_SIZE equ 8192
+
 section .bss                ; 'Block Started by Symbol' memory section to declare variable not initialised
-resb 8192                   ; 8KB for stack
+resb KERNEL_STACK_SIZE      ; 8KB for stack
 
 stack_space:
--- kernel.c
+++ kernel.c
@@ -92,22 +92,40 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
 }
 
 void terminal_putnewline(void) {
-    terminal_row ++;
+    terminal_row++;
     terminal_column = 0;
+}
+
+void terminal_scroll(void) {
+	
+	/* We drop one line */
+	size_t screen_size = VGA_WIDTH * VGA_HEIGHT;
+
+	/* Shift line row N on row N - 1  */
+	for (size_t i = 0, j = i + VGA_WIDTH; j < screen_size; i++, j++)
+		terminal_buffer[i] = terminal_buffer[j];
+	
+	/* Draw blank line down */
+	for (size_t i = screen_size - VGA_WIDTH; i < screen_size; i++)
+		terminal_buffer[i] = vga_entry(' ', terminal_color);
+
 }
 
 void terminal_putchar(char c) {
     /* Handle newline */
     if (c == '\n') {
         terminal_putnewline();
+		if (terminal_row == VGA_HEIGHT)
+			terminal_scroll();
         return;
     }
 
 	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
-		if (++terminal_row == VGA_HEIGHT)
-			terminal_row = 0;
+		terminal_putnewline();
+		if (terminal_row == VGA_HEIGHT)
+			terminal_scroll();
 	}
 }
 
@@ -151,7 +169,6 @@ void terminal_putheader(void) {
 }
 
 
-
 void kernel_main(void) {
 
 	terminal_initialize();
@@ -160,5 +177,9 @@ void kernel_main(void) {
     terminal_putheader();
 
     terminal_setcolor(0b00000010);
-	terminal_writestring("42");
+	//terminal_writestring("42");
+
+	//char *big_buffer = "Aa0Aa1Aa2Aa3Aa4Aa5Aa6Aa7Aa8Aa9Ab0Ab1Ab2Ab3Ab4Ab5Ab6Ab7Ab8Ab9Ac0Ac1Ac2Ac3Ac4Ac5Ac6Ac7Ac8Ac9Ad0Ad1Ad2Ad3Ad4Ad5Ad6Ad7Ad8Ad9Ae0Ae1Ae2Ae3Ae4Ae5Ae6Ae7Ae8Ae9Af0Af1Af2Af3Af4Af5Af6Af7Af8Af9Ag0Ag1Ag2Ag3Ag4Ag5Ag6Ag7Ag8Ag9Ah0Ah1Ah2Ah3Ah4Ah5Ah6Ah7Ah8Ah9Ai0Ai1Ai2Ai3Ai4Ai5Ai6Ai7Ai8Ai9Aj0Aj1Aj2Aj3Aj4Aj5Aj6Aj7Aj8Aj9Ak0Ak1Ak2Ak3Ak4Ak5Ak6Ak7Ak8Ak9Al0Al1Al2Al3Al4Al5Al6Al7Al8Al9Am0Am1Am2Am3Am4Am5Am6Am7Am8Am9An0An1An2An3An4An5An6An7An8An9Ao0Ao1Ao2Ao3Ao4Ao5Ao6Ao7Ao8Ao9Ap0Ap1Ap2Ap3Ap4Ap5Ap6Ap7Ap8Ap9Aq0Aq1Aq2Aq3Aq4Aq5Aq6Aq7Aq8Aq9Ar0Ar1Ar2Ar3Ar4Ar5Ar6Ar7Ar8Ar9As0As1As2As3As4As5As6As7As8As9At0At1At2At3At4At5At6At7At8At9Au0Au1Au2Au3Au4Au5Au6Au7Au8Au9Av0Av1Av2Av3Av4Av5Av6Av7Av8Av9Aw0Aw1Aw2Aw3Aw4Aw5Aw6Aw7Aw8Aw9Ax0Ax1Ax2Ax3Ax4Ax5Ax6Ax7Ax8Ax9Ay0Ay1Ay2Ay3Ay4Ay5Ay6Ay7Ay8Ay9Az0Az1Az2Az3Az4Az5Az6Az7Az8Az9Ba0Ba1Ba2Ba3Ba4Ba5Ba";
+	//terminal_writestring(big_buffer);
+
 }
