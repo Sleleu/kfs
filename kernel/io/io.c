769fa3b6fed031640855a962ee360932ae63e9d4
Sleleu <93100775+Sleleu@users.noreply.github.com>
2025-07-19 01:27:34 +0200

    add minimal backspace

--- kernel/io/io.c
+++ kernel/io/io.c
@@ -69,6 +69,15 @@ void terminal_putchar(char c) {
     /* Handle newline */
     if (c == '\n')
         terminal_putnewline();
+    else if (c == '\b') {
+        if (terminal_column == 0 && terminal_row != 0) {
+            terminal_row--;
+            terminal_column = VGA_WIDTH; // TODO: count number of spaces
+        }
+        else
+            terminal_column--;
+        terminal_putentryat(' ', terminal_color, terminal_column, terminal_row);
+    }
     else {
         terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
         if (++terminal_column == VGA_WIDTH) {
